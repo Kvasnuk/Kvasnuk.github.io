d3f5f3cfe588aeebb5d3c6bc06e7487a6a4f2929
Kvasnuk <kvasnuk.vasiliy@gmail.com>
2018-06-27 07:04:35 +0300

    Add News slider for Home page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,14 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-console.log('dd');
+jQuery(document).ready(function(){
+    $('#news-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        centerMode: true,
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite:true
+    });
+})
 },{}]},{},[1])
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log('dd');
+jQuery(document).ready(function(){
+    $('#news-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        centerMode: true,
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite:true
+    });
+})
